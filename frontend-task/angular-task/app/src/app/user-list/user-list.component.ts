dc36e345b68e0995cdb93c9b460ff68f58456326
Mariusz Najwer <najwer23@gmail.com>
2025-11-26 14:51:01 +0100

    Add user filtering and sorting functionality to user list component

--- frontend-task/angular-task/app/src/app/user-list/user-list.component.ts
+++ frontend-task/angular-task/app/src/app/user-list/user-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import {
   MatCell, MatCellDef,
@@ -8,19 +7,22 @@ import {
   MatHeaderCellDef,
   MatHeaderRow, MatHeaderRowDef,
   MatRow, MatRowDef,
-  MatTable, MatTableDataSource,
-} from '@angular/material/table'
-import { WebsocketService } from '../services/websocket.service'
-import {Router} from '@angular/router'
-import {Store} from '@ngrx/store'
-import {setCurrentUser} from '../store/store.actions'
+  MatTable, MatTableDataSource, MatTableModule
+} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import { UserService } from '../services/user.service';
+import { WebsocketService } from '../services/websocket.service';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { setCurrentUser } from '../store/store.actions';
 
 export interface UserModel {
-  id: number | string
-  name: any
-  role: any
-  email: any
-  protectedProjects: number
+  id: number | string;
+  name: any;
+  role: any;
+  email: any;
+  protectedProjects: number;
 }
 
 @Component({
@@ -38,20 +40,28 @@ export interface UserModel {
     MatCellDef,
     MatHeaderRowDef,
     MatRowDef,
+    MatPaginator,
+    MatTableModule,
+    MatSort,
+    MatSortModule
   ],
 })
 export class UserListComponent implements OnInit {
-  users: any
+  displayedColumns: string[] = ['name', 'role', 'protectedProjects'];
+  users = new MatTableDataSource<UserModel>([]);
 
   private userSub!: Subscription;
   private wsSub!: Subscription;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     public userService: UserService,
     public websocketService: WebsocketService,
     public router: Router,
     public store: Store,
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.loadUsers();
@@ -64,11 +74,21 @@ export class UserListComponent implements OnInit {
   loadUsers() {
     this.userSub = this.userService.getUsers().subscribe(data => {
       this.users = new MatTableDataSource(data);
+      this.users.paginator = this.paginator;  
+      this.users.sort = this.sort;   
     });
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.users.filter = filterValue.trim().toLowerCase();
+    if (this.users.paginator) {
+      this.users.paginator.firstPage();
+    }
+  }
+
   userDetails(user: UserModel) {
-    this.store.dispatch(setCurrentUser({user}))
-    this.router.navigate([user.id])
+    this.store.dispatch(setCurrentUser({ user }));
+    this.router.navigate([user.id]);
   }
 }
