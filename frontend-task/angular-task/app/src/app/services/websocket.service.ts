7a3e33122a99cc5ba823e50752b74569644edeaf
Mariusz Najwer <najwer23@gmail.com>
2025-11-27 00:22:52 +0100

    Refactor for websocket (ngOnDestroy)

--- frontend-task/angular-task/app/src/app/services/websocket.service.ts
+++ frontend-task/angular-task/app/src/app/services/websocket.service.ts
@@ -1,22 +1,34 @@
-import { Injectable, NgZone } from "@angular/core";
+import { Injectable, NgZone, OnDestroy } from "@angular/core";
 import { Observable, Subject } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
-export class WebsocketService {
-  private socket!: WebSocket;
-  subject = new Subject<string>();
+export class WebsocketService implements OnDestroy {
+  private socket?: WebSocket;
+  public subject = new Subject<string>();
 
   constructor(private ngZone: NgZone) {}
 
   public connect(url: string): Observable<string> {
+    if (this.socket) {
+      this.socket.close();
+    }
+
     this.socket = new WebSocket(url);
 
-    this.socket.onmessage = (event) => {
+    this.socket.onmessage = (event: MessageEvent) => {
       this.ngZone.run(() => {
         this.subject.next(event.data);
       });
+    };
+
+    this.socket.onerror = (event: Event) => {
+      console.error("WebSocket error:", event);
+    };
+
+    this.socket.onclose = (event: CloseEvent) => {
+      console.log(`WebSocket closed. Code: ${event.code}, Reason: ${event.reason}`);
     };
 
     return this.subject.asObservable();
@@ -25,6 +37,13 @@ export class WebsocketService {
   public sendMessage(msg: string) {
     if (this.socket && this.socket.readyState === WebSocket.OPEN) {
       this.socket.send(msg);
+    } else {
+      console.warn("WebSocket not open. Message not sent.");
     }
+  }
+
+  ngOnDestroy() {
+    this.socket?.close();
+    this.subject.complete();
   }
 }
